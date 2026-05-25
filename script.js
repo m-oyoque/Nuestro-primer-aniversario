b7ae93293c3b69d77a439712ca04b7e0b093b2d0
m-oyoque <ruizoyoquem@gmail.com>
2026-05-24 21:11:21 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+const fechaInicio = new Date("2024-05-31T00:00:00");
+
+function actualizarContador() {
+    const ahora = new Date();
+    const diferencia = ahora - fechaInicio;
+
+    const dias = Math.floor(diferencia / (1000 * 60 * 60 * 24));
+    const horas = Math.floor((diferencia / (1000 * 60 * 60)) % 24);
+    const minutos = Math.floor((diferencia / (1000 * 60)) % 60);
+
+    const contador = document.getElementById("contador");
+
+    if(contador){
+        contador.innerHTML =
+        `${dias} días ♡ ${horas} horas ♡ ${minutos} minutos`;
+    }
+}
+
+setInterval(actualizarContador, 1000);
+
 function revealLetter() {
-  document.getElementById("letter").classList.add("show");
+    document.getElementById("letter").classList.add("show");
 }
