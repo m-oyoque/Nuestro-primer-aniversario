cdf30e29016a3021af5dd98e708d7fab34515bf1
m-oyoque <ruizoyoquem@gmail.com>
2026-05-24 21:17:49 -0600

    Update script.js

--- script.js
+++ script.js
@@ -25,3 +25,15 @@ function revealLetter() {
 function mensajeSecreto(){
     document.getElementById("secretito").classList.add("show");
 }
+
+window.addEventListener("load", () => {
+    const loader = document.getElementById("loader");
+
+    if(loader){
+        loader.style.opacity = "0";
+
+        setTimeout(() => {
+            loader.style.display = "none";
+        },1000);
+    }
+});
